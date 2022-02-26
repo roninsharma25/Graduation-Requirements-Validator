2384382d7c6bf9014467ec922738ef747b473177
Ronin Sharma <rrs234@cornell.edu>
2022-02-25 23:56:27 -0500

    added basic app logic

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import numpy as np
+import streamlit as st
+import graphviz as graphviz
+import os
+
+
+st.title('Graduation Requirements Validator')
+
+
