2a75d638139298edec74c97407dd4710480d5d3e
Ronin Sharma <rrs234@cornell.edu>
2022-02-27 23:55:33 -0500

    started planning streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,11 @@ import os
 
 st.title('Graduation Requirements Validator')
 
+### Add template file download feature
 
+### Add file upload feature
+
+if st.button("Submit File"):
+    st.write("File Submitted")
+else:
+    st.write("File Not Submitted Yet")
