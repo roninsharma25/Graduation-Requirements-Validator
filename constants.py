e34d0f2213f530cc307cf1eb8291681f07e9eab8
Ronin Sharma <rrs234@cornell.edu>
2022-04-05 01:39:40 -0400

    added PE requirement checker

---
+++ constants.py
@@ -0,0 +1,19 @@
+# Total number of required classes
+NUM_FWS = 2
+NUM_PE = 2
+NUM_ENGRI = 1
+NUM_ENGRD = 1
+NUM_CDE = 1
+
+# Total number of required credits
+NUM_LS_CREDITS = 18
+
+# Classes satisfying core requirements
+CORE_CLASSES = {
+    'Math': [1910, 1920, 2930, 2940],
+    'Physics': [1112, 2213, 2214],
+    'Chemistry': [2090],
+    'CS': [1110]
+}
+
+
--- utils.py
+++ utils.py
@@ -1,7 +1,24 @@
+from constants import *
 import pandas as pd
 
 
+
 def analyzeData(uploadedFile):
-    fileData = pd.read_csv(uploadedFile)
+    problems = []
+
+    # TODO: the first non-header line shouldn't need to be the max length 
+    fileData = pd.read_csv(uploadedFile, names = ['Category', 'Class 1', 'Class 2', 'Class 3', 'Class 4'], header = 0, index_col = 'Category')
+
+    # Check PE requirement
+    fileData = fileData.loc[['PE']]
+
+    peClasses = [class_.strip() for class_ in fileData.iloc[0] if not pd.isnull(class_)]
+    numPEClasses = len(peClasses)
+    print(peClasses)
+
+    if (numPEClasses < NUM_PE):
+        problems.append(f'You have not taken enough PE classes. You have only taken {numPEClasses}. You have only taken {", ".join(peClasses)}.')
+
     print(fileData)
+    print(f'Problems: {problems}')
     return fileData
