a9a3f44490ade7e4736e76c38e9ad56c0db35af5
Ronin Sharma <rrs234@cornell.edu>
2022-05-01 16:54:48 -0400

    fixed multi-page functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,57 +1,41 @@
 from utils import *
 import streamlit as st
-import pageSelector
+
+pageTitles = ['Graduation Requirements Validator', 'Analyze Results']
+pageFormButtons = ['Submit', 'Back']
+
+if 'page' not in st.session_state or 'error' not in st.session_state:
+    st.session_state.page = 0
+    st.session_state.error = False
+
+st.title(pageTitles[st.session_state.page])
 
 
-page = st.button('Start')
+# Cases for each page
 
-if 'pageClicked' not in st.session_state:
-    st.session_state.pageClicked = 0
+if (st.session_state.page == 0):
+    ### Add template file download feature
+    if st.button('Download Template'):
+        print('download template')
 
-pages = ['Home', 'Download', 'View Results']
-
-if (page):
-    st.session_state.pageClicked += 1
-    st.session_state.pageClicked = 0 if st.session_state.pageClicked >= len(pages) else st.session_state.pageClicked
-
-
-print(st.session_state.pageClicked)
-st.write(pages[st.session_state.pageClicked])
-
-
-# application = pageSelector.PageSelector()
-# page = 0
-
-
-# def homePage():
-#     global page
-#     st.title('Graduation Requirements Validator')
-
-#     ### Add template file download feature
-
-#     ### Add file upload feature
-#     if (page == 0):
-#         button = st.button('Start')
-
-#         if not button:
-#             fileUploader = st.file_uploader('Select a file')
-
-#             if st.button('Submit File'):
-#                 if fileUploader is None:
-#                     st.write('You need to select a file')
-#                 else:
-#                     st.write('File submitted')
-#                     df = analyzeData(fileUploader)
-#                     st.write(df)
+    ### Add file upload feature
+    fileUploader = st.file_uploader('Select a file')
     
-#         if button:
-#             page = 1
-#             application.setPage(page)
+    if (st.session_state.error):
+        st.write('You need to select a file')
 
-# def downloadPage():
-#     st.title('Download Page')
+def buttonCallback():
+    if (st.session_state.page == 0):
+        if fileUploader is None:
+            st.session_state.error = not st.session_state.error
+        else:
+            st.write('File submitted')
+            df = analyzeData(fileUploader)
+            st.write(df)
+    
+            # Switch pages
+            st.session_state.page = 1 - st.session_state.page
 
-# application.createPage('Home Page', homePage)
-# application.createPage('Download Page', downloadPage)
 
-# application.setPage(page)
+with st.form(key = 'Form Button'):
+    submitButton = st.form_submit_button(label = pageFormButtons[st.session_state.page], on_click = buttonCallback)
